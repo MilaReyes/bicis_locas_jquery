21985871cb42fd74ae369d14a7959c7e22d54f15
milareyes <milena.reyes@gmail.com>
2016-11-16 01:46:12 -0300

    Variables Jquery, funciones jquery,pero sin funcionar

--- js/main.js
+++ js/main.js
@@ -11,24 +11,24 @@ $(document).ready(function(){
 		$('.name-container').append('<span class='erase' >Ingresa un nombre válido</span>');
 	}else if (name.substring(0,1) == name.substring(0,1).toUpperCase() == false){
 		$('.name-container').append('<span class='erase' >Primera letra en mayúscula</span>');
-	
+	}
 
     if( lastName == null || lastName.length <=2 || /^\s+$/.test(lastName) || /^[a-zA-Z]*$/.test(lastName)==false){
 		  $('.lastname-container').append('<span class="erase" >Ingresa un apellido válido</span>');
 	}else if (lastName.substring(0,1) == lastName.substring(0,1).toUpperCase() == false){
 	      $('.lastname-container').append('<span class="erase" >Primera letra en mayúscula</span>');
-      
+    }
 
     if (mail == null || mail.length < 0||/^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/.test(mail) != true){
 		$('email-container').append('<span class="erase" >Ingresa una dirección de correo electronico válida</span>');
-
-        if (password.length == 0 ){
-          $('.form-group:first').append('<span class="borrar" >Por favor escribe una contraseña</span>');      
-        } else if (password.length < 6){
-          $('.form-group:first').append('<span class="borrar" >Contraseña muy corta, minimo 6 caracteres</span>');  
-        } else if(password== 123456 || password== 098754){
-          $('.form-group:first').append('<span class="borrar" >Esta contraeña no es segura</span>');
-     
+    }
+    if (password.length == 0 ){
+          $('.form-group:first').append('<span class="erase" >Por favor escribe una contraseña</span>');      
+        }else if (password.length < 6){
+          $('.form-group:first').append('<span class="erase" >Contraseña muy corta, minimo 6 caracteres</span>');  
+        }else if(password== 123456 || password== 098754){
+          $('.form-group:first').append('<span class="erase" >Esta contraeña no es segura</span>');
+    } 
     if( biciTypes== null || biciTypes== 0 ){
-          $('.form-group:eq(1)').append('<span class="borrar" >Escoge tu tipo de bici!</span>')};  
-    
+          $('.form-group:eq(1)').append('<span class="erase" >Escoge tu tipo de bici!</span>')};  
+    }
