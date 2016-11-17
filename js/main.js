9b991db91ded0e3f7aedaa208c42d149a44684a8
milareyes <milena.reyes@gmail.com>
2016-11-16 23:58:30 -0300

    Aún sin funcionar jquery

--- js/main.js
+++ js/main.js
@@ -1,34 +1,32 @@
-$(document).ready(function(){
-    $('button').click(function(){
-        $('.erase').remove();
-	var name=$('#name').val();
-	var lastName=$('#lastname').val();
-	var mail=$('#input-email').val();
-	var password=$('#input-password').val();
-	var biciTypes= $('select').val();
 
-	if( name == null || name.length <=2 || /^\s+$/.test(name) || /^[a-zA-Z]*$/.test(name)==false) {
-		$('.name-container').append('<span class='erase' >Ingresa un nombre válido</span>');
-	}else if (name.substring(0,1) == name.substring(0,1).toUpperCase() == false){
-		$('.name-container').append('<span class='erase' >Primera letra en mayúscula</span>');
+function validateForm(){
+    var name=$('#name').val();
+	   if( name == null || name.length <=2 || /^\s+$/.test(name) || /^[a-zA-Z]*$/.test(name)==false) {
+       $('#name').parent().append('<span>Ingresa un nombre válido</span>');
+	}else if (name.substring(0,1) == name.substring(0,1).toUpperCase() == false){   
+		$('#name').parent()append('<span>Primera letra en mayúscula</span>');
 	}
 
+    var lastName=$('#lastname').val();
     if( lastName == null || lastName.length <=2 || /^\s+$/.test(lastName) || /^[a-zA-Z]*$/.test(lastName)==false){
-		  $('.lastname-container').append('<span class="erase" >Ingresa un apellido válido</span>');
+		  $('#name').parent().append('<span>Ingresa un apellido válido</span>');
 	}else if (lastName.substring(0,1) == lastName.substring(0,1).toUpperCase() == false){
-	      $('.lastname-container').append('<span class="erase" >Primera letra en mayúscula</span>');
+	      $('#lastname').parent().append('<span>Primera letra en mayúscula</span>');
     }
-
+    var mail=$('#input-email').val();
     if (mail == null || mail.length < 0||/^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/.test(mail) != true){
-		$('email-container').append('<span class="erase" >Ingresa una dirección de correo electronico válida</span>');
+		$('#input-email').parent().append('<span>Ingresa una dirección de correo electronico válida</span>');
     }
+    var mail=$('#input-password').val();
     if (password.length == 0 ){
-          $('.form-group:first').append('<span class="erase" >Por favor escribe una contraseña</span>');      
+         $('#input-password').parent().append('<span>Por favor escribe una contraseña</span>');      
         }else if (password.length < 6){
-          $('.form-group:first').append('<span class="erase" >Contraseña muy corta, minimo 6 caracteres</span>');  
+          $('#input-password').parent().append('<span>Contraseña muy corta, minimo 6 caracteres</span>');  
         }else if(password== 123456 || password== 098754){
-          $('.form-group:first').append('<span class="erase" >Esta contraeña no es segura</span>');
+          $('#input-password').parent().append('<span>Esta contraeña no es segura</span>');
     } 
+    var biciTypes= $('select').val();
     if( biciTypes== null || biciTypes== 0 ){
-          $('.form-group:eq(1)').append('<span class="erase" >Escoge tu tipo de bici!</span>')};  
-    }
+          $('select').parent().append('<span>Escoge tu tipo de bici!</span>')};  
+      }
+    
