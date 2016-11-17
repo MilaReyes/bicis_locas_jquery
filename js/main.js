d3741cc00664fab9d1aaad52362e2ea3937f8d85
milareyes <milena.reyes@gmail.com>
2016-11-17 12:45:14 -0300

    Listo al fin!

--- js/main.js
+++ js/main.js
@@ -4,12 +4,12 @@ function validateForm(){
 	   if( name == null || name.length <=2 || /^\s+$/.test(name) || /^[a-zA-Z]*$/.test(name)==false) {
        $('#name').parent().append('<span>Ingresa un nombre válido</span>');
 	}else if (name.substring(0,1) == name.substring(0,1).toUpperCase() == false){   
-		$('#name').parent()append('<span>Primera letra en mayúscula</span>');
+		$('#name').parent().append('<span>Primera letra en mayúscula</span>');
 	}
 
     var lastName=$('#lastname').val();
     if( lastName == null || lastName.length <=2 || /^\s+$/.test(lastName) || /^[a-zA-Z]*$/.test(lastName)==false){
-		  $('#name').parent().append('<span>Ingresa un apellido válido</span>');
+		  $('#lastname').parent().append('<span>Ingresa un apellido válido</span>');
 	}else if (lastName.substring(0,1) == lastName.substring(0,1).toUpperCase() == false){
 	      $('#lastname').parent().append('<span>Primera letra en mayúscula</span>');
     }
@@ -17,7 +17,7 @@ function validateForm(){
     if (mail == null || mail.length < 0||/^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/.test(mail) != true){
 		$('#input-email').parent().append('<span>Ingresa una dirección de correo electronico válida</span>');
     }
-    var mail=$('#input-password').val();
+    var password=$('#input-password').val();
     if (password.length == 0 ){
          $('#input-password').parent().append('<span>Por favor escribe una contraseña</span>');      
         }else if (password.length < 6){
